d9cd6e18a436517d729fccdba8a8d6e9f2dfabe0
RolyPolyCowboy <138080819+RolyPolyCowboy@users.noreply.github.com>
2026-05-14 15:47:53 -0700

    Update and rename leds (Not Updated).py to leds.py

--- src/robot/leds.py
+++ src/robot/leds.py
@@ -1,7 +1,8 @@
 import machine
-from ws2812 import WS2812
 import time
+from ws2812 import WS2812
 
+#LED Strip
 ws = WS2812(machine.Pin(28), 8)
 
 def show_solid(color):
@@ -25,21 +26,17 @@ def show_found_black():
 
 def show_obstacle():
     """Flash white - obstacle detected."""
-    show_flash([255, 255, 255], flashes=3)
+    show_flash([255, 255, 255], flashes=2)
 
-def show_found_white(color):
+def show_found_white():
     """White scanning pattern - following border, flashes the color its looking for."""
     for i in range(8):
         ws.write_all([0, 0, 0])
-        if color == Red:
-            ws[i] = [200, 0, 0]
-        elif color == Green:
-            ws[i] = [0, 200, 0]
-        else:
-            ws[i] = [0, 0, 200]
-        time.sleep_ms(10)
+        ws[i] = [200, 200, 200]
+        time.sleep_ms(20)
         ws.write()
-        time.sleep_ms(200)
+        time.sleep_ms(20)
+    ws.write_all([0, 0, 0])
         
 def show_found_red():
     """A red-themed celebration pattern."""
@@ -60,12 +57,12 @@ def show_found_red():
 
 def show_found_green():
     """A green-themed celebration pattern."""
-    for i in range(5):
+    for i in range(3):
         ws.write_all([0, 0, 0])
         show_flash([0,255,0],3,100,100)
         ws[i] = [255, 0, 0]
         time.sleep_ms(1000)
-        show_flash([0,255,0],3,100,100)
+        show_flash([0,255,0],2,50,100)
         time.sleep_ms(10)
         ws.write()
         time.sleep_ms(100)
@@ -82,32 +79,31 @@ def show_found_blue():
         ws[i] = [0, 0, 255]
         time.sleep_ms(100)
     pass
-# Test each pattern
-print("Testing white...")
-show_found_white()
-time.sleep(2)
 
-print("Testing obstacle...")
-show_obstacle()
-time.sleep(1)
-
-print("Testing black...")
-for _ in range(3):
-    show_found_black()
-
-print("Testing red...")
-show_found_red()
-time.sleep(1)
-
-print("Testing green...")
-show_found_green()
-time.sleep(1)
-
-print("Testing blue...")
-show_found_blue()
-time.sleep(1)
-
-# Turn off
-ws.write_all([0, 0, 0])
-ws.write()
-print("Done!")
+def finale_color():
+    ws.write_all([0, 0, 0])
+    show_flash([0,0,255],1,500,500)
+    show_flash([0,255,0],1,500,500)
+    show_flash([255,0,0],1,500,500)
+    for i in range(8):
+        ws[i] = [0, 0, 255]
+        time.sleep_ms(50)
+        ws.write()
+        time.sleep_ms(10)
+    time.sleep_ms(10)
+    for i in range(8):
+        ws[i] = [0, 255, 0]
+        time.sleep_ms(50)
+        ws.write()
+        time.sleep_ms(10)
+    time.sleep_ms(10)
+    for i in range(8):
+        ws[i] = [255, 0, 0]
+        time.sleep_ms(50)
+        ws.write()
+        time.sleep_ms(10)
+    time.sleep_ms(400)
+    show_flash([0,0,255],1,200,500)
+    show_flash([0,255,0],1,500,500)
+    show_flash([255,0,0],1,300,500)
+    show_flash([255, 255, 255],3,200,400)
