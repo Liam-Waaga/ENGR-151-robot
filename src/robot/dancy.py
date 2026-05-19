21460b9bcadba6921661f1ea8ac354287199da0c
michaelmckenzie816 <michaelmckenzie816@gmail.com>
2026-05-19 15:12:09 -0700

    dancy updates

--- src/robot/dancy.py
+++ src/robot/dancy.py
@@ -22,13 +22,14 @@ import machine
 import motion
 import time
 import leds
-import lcd1602 as LCD
+from lcd1602 import LCD
 from ws2812 import WS2812
 
 #LED Strip Setup
 ws = WS2812(machine.Pin(28), 8)
 
-
+#lcd Screen
+lcd = LCD()
 
 def clean_and_close(color=None):
     ''' makes sure everything is off when returning / prints saying finished with section'''
@@ -36,7 +37,7 @@ def clean_and_close(color=None):
     if color is not None: # if given a string, it prints it in a logging meassage
         print(f'finished {color} dance')
     ws.write_all([0, 0, 0]) # makes sure led is off
-    LCD.clear() # makes sure lcd is off
+    lcd.clear() # makes sure lcd is off
 
 
 
@@ -69,7 +70,7 @@ def panic(flashcount=3, shutdown=True):
     
     
     print('panic') #logs the start of panic
-    LCD.write('!!!PANIC!!!')
+    lcd.write(0, 0, '!!!PANIC!!!')
     
     for _ in range(flashcount): # plays sos in morse code
         leds.show_flash(*s)
@@ -92,27 +93,27 @@ def dance_green():
     
     print(text) # logs the start of green dance
     for i in range(8):
-        leds.show_flash([0,255,0],3,100,100) # 3 flash blue
+        leds.show_flash([0,255,0],3,100,50) # 3 flash blue
         ws[i] = [0, 255, 0]
-        ws.write() #write a single blue led at index i
+        ws.write() #write a single green led at index i
         
         if i % 3 == 1: #shows 'Found ' and every once in a while reveals 3 more characters
-            LCD.clear()
-            LCD.write(text[:(i+5)])
+            lcd.clear()
+            lcd.write(0, 0, text[:(i+5)])
         
         if i % 2 == 0: # drive back and forth
-            motion.drive_distance_mm(-50, 200)
+            motion.drive_mm(-5)
         else:
-            motion.drive_distance_mm(50, 200)
+            motion.drive_mm(5)
         
-        time.sleep_ms(100)
-        leds.show_flash([0,255,0],2,50,100) #2 flash blue
+        time.sleep_ms(50)
+        leds.show_flash([0,255,0],2,50,50) #2 flash green
         time.sleep_ms(100)
     clean_and_close('green') # logs the end of green dance
 
 
 
-def dance_blue(repeat=1):
+def dance_blue(repeat=0):
     ''' Blue Dance '''
     
     #takes in repeat:
@@ -124,7 +125,7 @@ def dance_blue(repeat=1):
     print(text) # log start of blue dance
     for i in range(repeat):
         ws.write_all([0, 0, 0])
-        LCD.write(text)
+        lcd.write(0, 0, text)
         
         for _ in range(5): # fades blue in and out 5 times
             for j in range(0,256,1):
@@ -134,12 +135,13 @@ def dance_blue(repeat=1):
                 ws.write_all([0, 0, j])
                 time.sleep_us(500)
                 
-        LCD.clear()
+        lcd.clear()
         leds.show_flash([50,0,100],3,100,100) #should this be lavender?
         time.sleep_ms(100)
-        motion.turn_deg(90, 200)
-        motion.turn_deg(-180, 200)
-        motion.turn_deg(90, 200)
+        
+        motion.turn_deg(90)
+        motion.turn_deg(-180)
+        motion.turn_deg(90)
     clean_and_close('blue') # log end of blue dance
 
 
@@ -151,32 +153,30 @@ def dance_red(pivots=2):
     #    (Default) otherwise: repeats 2 times
     
     text = 'Found Red!'
+    motorstrength = 40
     
     print(text)
-    LCD.write(text)
+    lcd.write(0, 0, text)
     for i in range(8):
         ws.write_all([0, 0, 0])
         ws[i] = [255, 0, 0]
         time.sleep_ms(50)
         ws.write()
         time.sleep_ms(80)
-    LCD.clear()
-    motion.drive_mm(-100)
+    lcd.clear()
+    motion.drive_mm(-50)
     
-    LCD.write(text)
+    lcd.write(0, 0, text)
     for i in range(8):
         ws.write_all([0, 0, 0])
-        ws[i] = [200, 0, 0]
-        time.sleep_ms(200)
+        ws[i] = [255, 0, 0]
+        time.sleep_ms(50)
         ws.write()
-        time.sleep_ms(200)
-    LCD.clear()
-    motion.drive_mm(100)
+        time.sleep_ms(80)
+    lcd.clear()
+    motion.drive_mm(50)
     
-    leds.show_flash([255,0,0],3,500,500)
-    
-    LCD.write(text)
-    motorstrength = 40
+    lcd.write(0, 0, text)
     motion.turn_deg(5, motorstrength)
     for _ in range(pivots):
         motion.turn_deg(-10, motorstrength)
@@ -203,65 +203,65 @@ def victory(endspincount=5,shutdown=False):
     
     #prep
     ws.write_all([0, 0, 0])
-    LCD.clear()
+    lcd.clear()
     text = '    ' + 'Victory!' #second set should be 8 characters to look good
     endspincountdeg = endspincount * (-360)
     
     
     #the real stuff
     print('Victory Time!')
-    LCD.write(text)
+    lcd.write(0, 0, text)
     leds.show_flash([0,0,255],1,500,500)
     leds.show_flash([0,255,0],1,500,500)
     leds.show_flash([255,0,0],1,500,500)
-    LCD.clear()
+    lcd.clear()
     
     for i in range(8):
-        LCD.write(text[:(i+5)])
+        lcd.write(0, 0, text[:(i+5)])
         ws[i] = [0, 0, 255]
         time.sleep_ms(50)
         ws.write()
         time.sleep_ms(10)
-        motion.turn_deg(5, 200)
-        LCD.clear()
+        motion.turn_deg(5)
+        lcd.clear()
         
     time.sleep_ms(10)
     
     for i in range(8):
-        LCD.write((i+4)*' ' + text[(i+4):])
+        lcd.write(0, 0, (i+4)*' ' + text[(i+4):])
         ws[i] = [0, 255, 0]
         time.sleep_ms(50)
         ws.write()
         time.sleep_ms(10)
-        motion.turn_deg(5, 200)
-        LCD.clear()
+        motion.turn_deg(5)
+        lcd.clear()
         
     time.sleep_ms(10)
     
     for i in range(8):
-        LCD.write(text[:(i+5)])
+        lcd.write(0, 0, text[:(i+5)])
         ws[i] = [255, 0, 0]
         time.sleep_ms(50)
         ws.write()
         time.sleep_ms(10)
-        motion.turn_deg(5, 200)
-        LCD.clear()
+        motion.turn_deg(5)
+        lcd.clear()
         
-    time.sleep_ms(400)
+    time.sleep_ms(300)
     
     def flashtext(color):
-        LCD.write(text)
-        leds.show_flash(color,1,500,0)
-        LCD.clear()
-        time.sleep_ms(500)
+        lcd.write(0, 0, text)
+        leds.show_flash(color,1,250,0)
+        lcd.clear()
+        time.sleep_ms(250)
     
     flashtext([255,0,0])
     flashtext([0,255,0])
     flashtext([0,0,255])
 
-    LCD.write(text)
+    lcd.write(0, 0, text)
     leds.show_flash([255, 255, 255],3,200,200)
-    motion.turn_deg(endspincountdeg, 200)
+    motion.turn_deg(endspincountdeg)
     clean_and_close('victory')
     
     if shutdown is True:
@@ -275,7 +275,7 @@ def victory(endspincount=5,shutdown=False):
         ws[7] = [255, 255, 255]  # White
         
         print('Shutting Down')
-        LCD.write('ShuttingDown')
+        lcd.write(0, 0, 'ShuttingDown')
         ws.write()
 
         for i in range(-1,-9,-1):
